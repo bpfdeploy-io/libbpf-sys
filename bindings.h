e69df8078c43b3a6ba1e1c78dc2595810cdc2cba
Milan <milan@mdaverde.com>
2022-04-18 10:05:32 -0400

    bpf insn macro helpers
    Signed-off-by: Milan <milan@mdaverde.com>
    
    removes bpf_helper_func_names

--- bindings.h
+++ bindings.h
@@ -10,4 +10,15 @@
 #include "libbpf/src/bpf.h"
 #include "libbpf/src/btf.h"
 #include "libbpf/src/libbpf.h"
+#include "libbpf/include/linux/filter.h"
 #endif /* __LIBBPF_SYS_NOVENDOR */
+
+extern struct bpf_insn _BPF_MOV64_IMM(__u8 dst, __s32 imm);
+
+extern struct bpf_insn _BPF_ALU64_IMM(__u8 op, __u8 dst, __s32 imm);
+
+extern struct bpf_insn _BPF_JMP_IMM(__u8 op, __u8 dst, __s32 imm, __s16 off);
+
+extern struct bpf_insn _BPF_JMP32_IMM(__u8 op, __u8 dst, __s32 imm, __s16 off);
+
+extern struct bpf_insn _BPF_EXIT_INSN();
